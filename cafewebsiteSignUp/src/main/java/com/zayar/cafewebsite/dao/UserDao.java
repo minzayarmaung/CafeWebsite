9838714486efc82c7f2e132ed16696c4ec645b9e
Min Zayar Maung <94619791+minzayarmaung@users.noreply.github.com>
2023-11-04 11:30:49 +0630

    Signup Component
    Signup Backend for CafeWebsite

---
+++ cafewebsiteSignUp/src/main/java/com/zayar/cafewebsite/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.zayar.cafewebsite.dao;
+
+import com.zayar.cafewebsite.POJO.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface UserDao extends JpaRepository<User,Integer> {
+
+    User findByEmailId(@Param("email") String email);
+
+
+}
---
+++ cafewebsiteSignUp/src/main/java/com/zayar/cafewebsite/restImpl/UserRestImpl.java
@@ -0,0 +1,31 @@
+package com.zayar.cafewebsite.restImpl;
+
+import com.zayar.cafewebsite.constents.CafeConstants;
+import com.zayar.cafewebsite.rest.UserRest;
+import com.zayar.cafewebsite.service.UserService;
+import com.zayar.cafewebsite.utils.CafeUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class UserRestImpl implements UserRest {
+
+    @Autowired
+    UserService userService;
+
+    @Override
+    public ResponseEntity<String> signUp(Map<String, String> requestMap) {
+        try{
+            return userService.signUp(requestMap);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG,
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
